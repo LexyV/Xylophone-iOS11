41e55cace3a09bfd4ab6073ab323fe80e3ddc217
Lexy Vanderford <alexysvanderford@gmail.com>
2018-05-15 16:46:14 -0400

    implemented sound

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -1,29 +1,63 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 27/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
-//
 
-import UIKit
+//always necessary
+import UIKit;
 
-class ViewController: UIViewController{
+//Step 1
+//audio visual foundation *works with the longer version of code*
+import AVFoundation;
+
+//Step 2 - add (,AVAudioPlayerDelegate
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+
+    //Step 3 - create variable w/ type av audio player w/ !
+    var audioPlayer: AVAudioPlayer!;
     
-
+    //make global variable
+    var selectedSoundFileName : String = "";
+    
+    //create array of all the sounds (.wav not necessary)
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"];
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
     }
-
-
 
     @IBAction func notePressed(_ sender: UIButton) {
+        //used to pick out exact note to sound play *use - 1 so you don't get out of range error*
+        selectedSoundFileName = soundArray[sender.tag - 1];
         
-        print(sender.tag)
-        
+        playSound();
     }
     
-  
-
+    func playSound() {
+        
+        //Step 4 - create url for when sound pressed
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer.play()
+        
+        }
 }
+
+
+
+//works with the shorter version of code
+//import AudioToolbox;
+
+//search stack overflow for (create and play sound in swift3) look for ones w green answers
+//Or search forums.developer.apple.com
+
+//shorter more updated version of code
+//        if let soundURL = Bundle.main.url(forResource: "note1", withExtension: "wav") {
+//            var mySound: SystemSoundID = 0
+//            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+//            //Play
+//            AudioServicesPlaySystemSound(mySound);
+//        }
 
